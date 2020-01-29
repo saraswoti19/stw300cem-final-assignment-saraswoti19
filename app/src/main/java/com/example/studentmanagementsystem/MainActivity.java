ffd3c2f6195c2960f007dedc9f75b44769a0bf7a
Saraswoti19 <sarastha16@gmail.com>
2020-01-29 19:29:49 +0545

    MainActivity

--- app/src/main/java/com/example/studentmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/studentmanagementsystem/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.studentmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new MyThread().start();
+    }
+    public class MyThread extends Thread{
+        public void run(){
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+            }
+            finally {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
     }
 }
